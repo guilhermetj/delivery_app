b7ccfe2263207ba0195332e36d825804f1e51951
StudiosTI <guips1998@gmail.com>
2023-02-01 17:52:35 -0300

    Finalizando Splash_Page

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,7 +1,8 @@
-import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:flutter/material.dart';
 
-import '../../core/config/env/env.dart';
+import '../../core/ui/widgets/delivery_button.dart';
+
 
 class SplashPage extends StatelessWidget {
   const SplashPage({ super.key });
@@ -9,22 +10,39 @@ class SplashPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),
-           ),
-           body: Column(
-             children: [
-               Container(),
-               DeliveryButton(
-                width: 200,
-                height: 200,
-                label: Env.i['backend_base_url'] ?? '',
-                onPressed: (){},
-               ),
-               TextFormField()
-             ],
+           body: ColoredBox(
+            color: const Color(0xFF140E0E),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    width: context.screenWidth,
+                    child: Image.asset('assets/images/lanche.png',fit: BoxFit.cover),
+                    ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: context.percentHeight(.30),
+                      ),
+                      Image.asset('assets/images/logo.png'),
+                      const SizedBox(
+                        height: 80,
+                      ),
+                      DeliveryButton(
+                        width: context.percentWidth(.5),
+                        height: 35,
+                        label: 'ACESSAR', 
+                        onPressed: () {},
+                        )
+                    ],
+                  ),
+                )
+              ],
+            ),
            ),
        );
   }
-
-
 }
