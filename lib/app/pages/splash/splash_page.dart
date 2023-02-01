b4041486759461ec5fa99a5f68d1da1466c0d31d
StudiosTI <guips1998@gmail.com>
2023-02-01 16:15:06 -0300

    Configurando extensions de ui e o tema

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,16 +1,22 @@
+import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
 import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
-
   const SplashPage({ super.key });
 
    @override
    Widget build(BuildContext context) {
-     
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
+           appBar: AppBar(title: const Text('Splash'),
+           ),
+           body: Column(
+             children: [
+               Container(),
+               ElevatedButton(onPressed: (){}, child: Text('Teste')),
+               TextFormField()
+             ],
+           ),
        );
   }
 
