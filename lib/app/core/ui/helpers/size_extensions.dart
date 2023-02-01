5022abc73e1c0ebeab91b357082f22608279664a
StudiosTI <guips1998@gmail.com>
2023-02-01 17:39:29 -0300

    Finalizando helper SizeExtensions

--- lib/app/core/ui/helpers/size_extensions.dart
+++ lib/app/core/ui/helpers/size_extensions.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 extension SizeExtensions on BuildContext{
   double get screenWidth => MediaQuery.of(this).size.width;
   double get screenHeight => MediaQuery.of(this).size.height;
+
+ //Ex: quero que tal coisa ocupe uma porcentagem da tela 
+  double percentWidth(double percent) => screenWidth * percent;
+  double percentHeight(double percent) => screenHeight * percent;
 }
