27603f20a07489cf3740ef923cd73d9b422b2372
StudiosTI <guips1998@gmail.com>
2023-02-02 17:33:50 -0300

    Criando status da home

---
+++ lib/app/pages/home/home_controller.dart
@@ -0,0 +1,7 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:delivery_app/app/pages/home/home_state.dart';
+
+class HomeController extends Cubit<HomeState>{
+  HomeController(super.initialState);
+}
