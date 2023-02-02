1fd6bd25acc8447fd21f1bf76f2a406766fe9518
StudiosTI <guips1998@gmail.com>
2023-02-02 16:48:28 -0300

    Iniciado home_page

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +10,18 @@ class HomePage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Center(child: Text('teste'),),
+           appBar: DeliveryAppbar(),
+           body: Column(
+            children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index){
+                  return DeliveryProductTile(product: ProductModel(id: 0, name: 'Lanche X', description: 'Lanche acompanha pão, calabresa, vinagrete, queijo e maionese', price: 15.0, image: 'https://i.ytimg.com/vi/0Fea2vwfnN8/maxresdefault.jpg'),);
+                },
+              ),
+            )
+           ]),
        );
   }
 }
